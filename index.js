f7eee6ce555fc43b11fcb93c5fc647d815263b11
Kristen Leach <kristenleach24@gmail.com>
2017-11-19 20:17:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var recipes = new Object({'prop': 1});
 
 function updateObjectWithKeyAndValue(object, key, value){
+  var emptyObj = {}
+  emptyObj[key]=value
   var newObj = Object.assign({}, object, emptyObj);
   return newObj;
 }
