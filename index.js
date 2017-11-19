52d333b23042967970c0c841e920b96efd8861e3
Kristen Leach <kristenleach24@gmail.com>
2017-11-19 18:54:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = new Object({'flour': '3 cups', 'eggs': '2', 'sugar': '1 cup'});
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign({'salt': 'pinch', recipes});
+  Object.assign({'salt': 'pinch'}, recipes);
   return recipes;
 }
