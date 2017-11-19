84c0238231da2aa3f3bb79332e329922602eb5db
Kristen Leach <kristenleach24@gmail.com>
2017-11-19 19:15:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
- return recipes.prop2 = 2;
+ return recipes['prop2'] = '2';
 }
